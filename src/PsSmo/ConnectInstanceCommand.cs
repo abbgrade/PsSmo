1178e90b4af55a51c25d79bfbcdb71ddb64050a3
Steffen <skampmann@munichre.com>
2021-10-07 10:24:25 +0200

    added output event handler

--- src/PsSmo/ConnectInstanceCommand.cs
+++ src/PsSmo/ConnectInstanceCommand.cs
@@ -167,7 +167,41 @@ namespace PsSmo
                     Instance = new Server();
                     break;
             }
+
+            Instance.ConnectionContext.InfoMessage += ConnectionContext_InfoMessage;
+            Instance.ConnectionContext.RemoteLoginFailed += ConnectionContext_RemoteLoginFailed;
+            Instance.ConnectionContext.ServerMessage += ConnectionContext_ServerMessage;
+            Instance.ConnectionContext.StateChange += ConnectionContext_StateChange;
+            Instance.ConnectionContext.StatementExecuted += ConnectionContext_StatementExecuted;
+
             WriteObject(Instance);
+        }
+
+        private void ConnectionContext_RemoteLoginFailed(object sender, ServerMessageEventArgs e)
+        {
+            WriteWarning(e.ToString());
+        }
+
+        private void ConnectionContext_StatementExecuted(object sender, StatementEventArgs e)
+        {
+            WriteVerbose(e.ToString().Trim());
+        }
+
+        private void ConnectionContext_StateChange(object sender, System.Data.StateChangeEventArgs e)
+        {
+            WriteVerbose($"Database state changed from {e.OriginalState} to {e.CurrentState}.");
+        }
+
+        private void ConnectionContext_ServerMessage(object sender, ServerMessageEventArgs e)
+        {
+            WriteWarning(e.ToString());
+        }
+
+        private void ConnectionContext_InfoMessage(object sender, SqlInfoMessageEventArgs e)
+        {
+            var message = e.Message.Trim();
+            if (message.Length > 0)
+                WriteVerbose(message);
         }
 
     }
