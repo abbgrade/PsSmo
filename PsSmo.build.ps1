ff10f18b6f2a098b48e2e3b038b198020a70c309
Steffen Kampmann <steffen.kampmann@gmail.com>
2022-02-20 15:03:23 +0100

    update build

--- PsSmo.build.ps1
+++ PsSmo.build.ps1
@@ -3,15 +3,33 @@
 	Build script <https://github.com/nightroman/Invoke-Build>
 
 .Example
-	Invoke-Build Publish -Configuration Release -NuGetApiKey xyz
+    # Create module from source.
+    Invoke-Build Build
+
+.Example
+    # Add doc templates for new command.
+    # BE CAREFUL! Existing documents will be overwritten and must be discarded using git.
+    Invoke-Build Doc.Init -ForceDocInit
+
 #>
 
 param(
 	[ValidateSet('Debug', 'Release')]
 	[string] $Configuration = 'Debug',
 
-	[string] $NuGetApiKey = $env:nuget_apikey
+	[string] $NuGetApiKey = $env:nuget_apikey,
+
+	# Overwrite published versions
+	[switch] $ForcePublish,
+
+    # Add doc templates for new command.
+	[switch] $ForceDocInit,
+
+	# Version suffix to prereleases
+	[int] $BuildNumber
 )
+
+$ModuleName = 'Pssmo'
 
 . $PSScriptRoot\tasks\Build.Tasks.ps1
 
