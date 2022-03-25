873fe32101542b92a75bb8c02cdde27f432ecea8
Steffen Kampmann <steffen.kampmann@gmail.com>
2022-03-25 18:16:35 +0100

    changed version

--- tasks/Dependencies.Tasks.ps1
+++ tasks/Dependencies.Tasks.ps1
@@ -6,7 +6,7 @@ task InstallTestDependencies -Jobs {
     Install-Module PsSqlTestServer -Scope CurrentUser -AllowPrerelease -Verbose
     Install-Module psdocker -Scope CurrentUser -AllowPrerelease -Verbose
     Install-Module PsSqlLocalDb -Scope CurrentUser -AllowPrerelease -Verbose
-    Install-Module PsSqlClient -Scope CurrentUser  -ErrorAction Stop -AllowPrerelease -Verbose
+    Install-Module PsSqlClient -Scope CurrentUser -ErrorAction Stop -MaximumVersion '0.4.0' -Verbose
 }
 
 task InstallReleaseDependencies -Jobs {
